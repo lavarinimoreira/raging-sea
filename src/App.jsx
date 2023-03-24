51be2b9033e0148e74a5cad9661da0a6bc8b18b9
Gabriel Lavarini <gabriel@lavarinimoreira.me>
2023-03-24 16:32:26 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import MyShaderMaterial from "./components/MyShaderMaterial";
+import React from "react";
+
+const App = () => {
+  return <MyShaderMaterial />;
+};
+export default App;
