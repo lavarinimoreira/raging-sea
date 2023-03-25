8185ee5381ec3fdde0e4d12aef41755052bc9f2a
Gabriel Lavarini <gabriel@lavarinimoreira.me>
2023-03-25 17:01:39 -0300

    animating

--- src/components/WaterShader.jsx
+++ src/components/WaterShader.jsx
@@ -3,23 +3,32 @@ import { useFrame } from "react-three-fiber";
 import waterVertexShader from "./../shaders/water/vertex.glsl";
 import waterFragmentShader from "./../shaders/water/fragment.glsl";
 import { Color, Texture, TextureLoader, Vector2 } from "three";
+import { useControls } from "leva";
 
 const WaterShader = () => {
-  // const materialRef = useRef();
+  const materialRef = useRef();
 
   // getting the shaders:
   const vertexShader = waterVertexShader;
   const fragmentShader = waterFragmentShader;
+
   const uniforms = {
+    uTime: { value: 0 },
     uBigWavesElevation: { value: 0.2 },
+    uBigWavesFrequency: { value: new Vector2(4.0, 1.5) },
+    uBigWavesSpeed: { value: 0.55 },
   };
 
-  return (
-    // <rawShaderMaterial/>
+  // Using the time to animate the shader
+  useFrame(({ clock }) => {
+    if (materialRef.current) {
+      materialRef.current.uniforms.uTime.value = clock.elapsedTime;
+    }
+  });
 
+  return (
     <shaderMaterial
-      // ref={materialRef}
-      // uniforms={uniforms}
+      ref={materialRef}
       vertexShader={vertexShader}
       fragmentShader={fragmentShader}
       uniforms={uniforms}
--- src/shaders/water/vertex.glsl
+++ src/shaders/water/vertex.glsl
@@ -1,10 +1,16 @@
+uniform float uTime;
 uniform float uBigWavesElevation;
+uniform vec2 uBigWavesFrequency;
+uniform float uBigWavesSpeed;
 
 void main()
 {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-    float elevation = sin(modelPosition.x) * uBigWavesElevation;
+    // Elevation
+    float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) *
+                      sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) *
+                      uBigWavesElevation;
 
     modelPosition.y += elevation;
 
