7d8404b16db930f58e9104de085b1c13886ec671
Gabriel Lavarini <gabriel@lavarinimoreira.me>
2023-03-26 12:32:21 -0300

    updating app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import "./App.css";
 import WaterShader from "./components/WaterShader";
 import React from "react";
+import { Canvas } from "react-three-fiber";
 
 const App = () => {
-  return <WaterShader />;
+  return (
+    <div className="main">
+      <Canvas camera={{ fov: 75, near: 0.1, far: 100, position: [1, 0.25, 1] }}>
+        <fog attach="fog" args={["#ffffff", 1, 5]} />
+        <mesh /*scale-y={2 / 3}*/ rotation-x={-Math.PI * 0.5}>
+          <planeBufferGeometry attach="geometry" args={[3.9, 3.9, 512, 512]} />
+          <WaterShader />;
+        </mesh>
+      </Canvas>
+      <footer className="footer">
+        Built by{" "}
+        <a href="https://lavarinimoreira.me" rel="noreferrer" target="_blank">
+          Gabriel Lavarini
+        </a>
+      </footer>
+    </div>
+  );
 };
+
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Canvas } from "react-three-fiber";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <div className="main">
-    <Canvas camera={{ fov: 75, near: 0.1, far: 100, position: [1, 0.25, 1] }}>
-      <fog attach="fog" args={["#ffffff", 1, 5]} />
-      <mesh /*scale-y={2 / 3}*/ rotation-x={-Math.PI * 0.5}>
-        <planeBufferGeometry attach="geometry" args={[3.9, 3.9, 512, 512]} />
-        <App />
-      </mesh>
-    </Canvas>
-    <footer className="footer">
-      Built by{" "}
-      <a href="https://lavarinimoreira.me" rel="noreferrer" target="_blank">
-        Gabriel Lavarini
-      </a>
-    </footer>
-  </div>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
